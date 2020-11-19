f8a069fe43082fababc8da2b26bbd1007ebc63c7
Eric Harris-Braun <eric@harris-braun.com>
2020-11-19 16:16:09 -0500

    fix constant

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const ADMIN_PORT = 4444;
 export const HAPP_PORT = 42233;
-export const UI_STORE_FOLDER = "/home/eric/code/metacurrency/holochain/self-hosted-happs-node/test/uis";
+export const UI_STORE_FOLDER = "/var/lib/self-hosted-happs/uis";
